b8207a68758cec9e58f544b09685272bdfdf87f3
ReimerGhost <rchamale10@gmail.com>
2020-02-01 11:51:33 -0600

    Cartas en accion

---
+++ RepairGGJ20/Assets/Scripts/Card.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : ScriptableObject
+{
+    public new string name;
+    public string description;
+
+    public Sprite artwork;
+
+    
+}
